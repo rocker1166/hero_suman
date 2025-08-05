b3da524f8cfcc6957694be49088e089cf66e1e20
SUMAN JANA <sumanjanaled@gmail.com>
2025-08-05 22:48:18 +0530

    Add new logo images 7.png and 8.png to the public/logo directory

--- app/page.tsx
+++ app/page.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from '@/components/layout/Header';
 import Hero from '@/components/sections/Hero';
+import LogoSection from '@/components/sections/LogoSection';
 
 
 export default function Home() {
@@ -8,7 +9,8 @@ export default function Home() {
     <div className="min-h-screen bg-white">
       <Header />
       <main>
-       <Hero />
+        <Hero />
+        <LogoSection />
       </main>
     </div>
   );
---
+++ components/sections/LogoSection.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+
+export default function LogoSection() {
+  return (
+    <section className="w-full bg-gray-50">
+      <div className="w-[1351px] h-32 relative bg-gray-50 mx-auto">
+        <div className="w-[1146.12px] h-8 pr-9 left-[102.44px] top-[45.52px] absolute inline-flex justify-start items-start gap-8">
+          <img className="w-28 h-8 relative" src="/logo/1.png" alt="Partner Logo 1" />
+          <img className="w-32 h-8 relative" src="/logo/2.png" alt="Partner Logo 2" />
+          <img className="w-32 h-8 relative" src="/logo/3.png" alt="Partner Logo 3" />
+          <img className="w-36 h-8 relative" src="/logo/4.png" alt="Partner Logo 4" />
+          <img className="w-28 h-8 relative" src="/logo/5.png" alt="Partner Logo 5" />
+          <img className="w-24 h-8 relative" src="/logo/6.png" alt="Partner Logo 6" />
+          <img className="w-36 h-8 relative" src="/logo/7.png" alt="Partner Logo 7" />
+          <img className="w-16 h-8 relative" src="/logo/8.png" alt="Partner Logo 8" />
+        </div>
+      </div>
+    </section>
+  );
+}
